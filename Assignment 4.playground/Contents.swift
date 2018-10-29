6e170db3fa48debfa5188fa108e8cd009dbb97b9
Andrew Robinson <sirarkimedes@gmail.com>
2018-10-28 20:40:07 -0500

    Add Newton polynomial

--- Assignment 4.playground/Contents.swift
+++ Assignment 4.playground/Contents.swift
@@ -18,6 +18,7 @@ let points = [Point(x: 0, y: 2),
               Point(x: 5, y: 9.1875),
               Point(x: 6, y: 12)]
 
+
 func lagrangeApproximate(x: Double) -> Double {
     func generateMultiplier(for position: Int) -> Double {
         var top = 1.0
@@ -40,4 +41,38 @@ func lagrangeApproximate(x: Double) -> Double {
     return value
 }
 
-lagrangeApproximate(x: 2)
+func newtonApproximate(x: Double, of degree: Int) -> Double {
+    func dividedDifference(for values: [Int]) -> Double {
+        if values.count == 1 {
+            return points[values[0]].y
+        } else {
+            var valuesRight = Array(values)
+            var valuesLeft = Array(values)
+            valuesRight.removeFirst()
+            valuesLeft.removeLast()
+
+            return (dividedDifference(for: valuesRight) - dividedDifference(for: valuesLeft)) / (points[values.last!].x - points[values.first!].x)
+        }
+    }
+
+    if degree - 1 == 1 {
+        return points[0].y + dividedDifference(for: [0, 1]) * (x - points[0].x)
+    } else {
+        var multiple = 1.0
+        for i in 0...degree - 2 {
+            multiple *= x - points[i].x
+        }
+
+        var ddValues = [Int]()
+        for i in 0...degree - 1 {
+            ddValues.append(i)
+        }
+        print(ddValues)
+        print(multiple)
+
+        return newtonApproximate(x: x, of: degree - 1) + dividedDifference(for: ddValues) * multiple
+    }
+}
+
+lagrangeApproximate(x: 4)
+newtonApproximate(x: 4, of: 6)
