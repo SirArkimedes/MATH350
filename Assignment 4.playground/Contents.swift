eb9df3f43203e35ba491c7bfbe72ac99b52a2312
Andrew Robinson <sirarkimedes@gmail.com>
2018-10-30 20:22:21 -0500

    Update Lagrange
    ALSO updates Newton’s for correct degree

--- Assignment 4.playground/Contents.swift
+++ Assignment 4.playground/Contents.swift
@@ -16,27 +16,72 @@ let points = [Point(x: 0.0, y: -6.0),
               Point(x: 0.6, y: -5.17788),
               Point(x: 1.0, y: -4.28172)]
 
+// Extend double to handle rounding.
+extension Double {
+    func round(toPlaces: Int) -> Double {
+        return (self * pow(10.0, Double(toPlaces))).rounded() / pow(10.0, Double(toPlaces))
+    }
+}
 
-func lagrangeApproximate(x: Double) -> Double {
-    func generateMultiplier(for position: Int) -> Double {
-        var top = 1.0
-        var bottom = 1.0
+
+func lagrange() {
+    func topOfMultiplier(for position: Int) -> String {
+        var top = ""
+
+        for (i, point) in points.enumerated() {
+            if i != position {
+                top += "(x - \(point.x))"
+            }
+        }
+        return top
+    }
+    func bottomOfMultiplier(for position: Int) -> String {
+        var bottom = ""
         let pointAtPosition = points[position]
 
         for (i, point) in points.enumerated() {
             if i != position {
-                top *= x - point.x
-                bottom *= pointAtPosition.x - point.x
+                bottom += "(\(pointAtPosition.x) - \(point.x))"
             }
         }
-        return top / bottom
+        return bottom
     }
 
-    var value = 0.0
-    for (i, point) in points.enumerated() {
-        value += generateMultiplier(for: i) * point.y
+    // Top
+    for i in 0...points.count - 1 {
+        if i == 0 {
+            print("          ", terminator: "")
+        } else {
+            print("             ", terminator: "")
+        }
+        print(topOfMultiplier(for: i).padding(toLength: bottomOfMultiplier(for: i).count, withPad: " ", startingAt: 0), terminator: "")
     }
-    return value
+    print()
+
+    // Middle
+    for (i, point) in points.enumerated() {
+        let multiplier = String(format: "%.6f ", point.y)
+        print(multiplier, terminator: "")
+        for _ in 1...bottomOfMultiplier(for: i).count {
+            print("-", terminator: "")
+        }
+
+        if i != points.count - 1 {
+            print(" + ", terminator: "")
+        }
+    }
+    print()
+
+    // Top
+    for i in 0...points.count - 1 {
+        if i == 0 {
+            print("          ", terminator: "")
+        } else {
+            print("             ", terminator: "")
+        }
+        print(bottomOfMultiplier(for: i), terminator: "")
+    }
+    print()
 }
 
 func newton(of degree: Int) -> String {
@@ -57,12 +102,12 @@ func newton(of degree: Int) -> String {
         return "\(points[0].y) + \(dividedDifference(for: [0, 1]))(x - \(points[0].x))"
     } else {
         var multiple = ""
-        for i in 0...degree - 2 {
+        for i in 0...degree - 1 {
             multiple += "(x - \(points[i].x))"
         }
 
         var ddValues = [Int]()
-        for i in 0...degree - 1 {
+        for i in 0...degree {
             ddValues.append(i)
         }
 
@@ -71,7 +116,8 @@ func newton(of degree: Int) -> String {
 }
 
 print("Lagrange's polynomial:")
-lagrangeApproximate(x: 4)
+print("P4(x) =")
+lagrange()
 print()
 print("Newton's polynomial:")
 print("P4(x) = " + newton(of: 4))
