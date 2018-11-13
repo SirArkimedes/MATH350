2fe593786fe024346862a6dbdf6d84b99d9a1a52
Andrew Robinson <sirarkimedes@gmail.com>
2018-11-13 13:46:23 -0600

    Set up stop condition

--- Assignment 5.playground/Contents.swift
+++ Assignment 5.playground/Contents.swift
@@ -26,9 +26,42 @@ func formulaThree(_ k: Double) -> Double {
 func formulaTen(_ k: Double) -> Double {
     return (-f(x: xo + 2 * h(k)) + 8 * f(x: xo + h(k)) - 8 * f(x: xo - h(k)) + f(x: xo - 2 * h(k))) / (12 * h(k))
 }
-for k in 1...10 {
-    print("Two: \(formulaTwo(Double(k)))")
-    print("Three: \(formulaThree(Double(k)))")
-    print("Ten: \(formulaTen(Double(k)))")
-    print()
+
+var formulaTwoValues = [Double]()
+var formulaThreeValues = [Double]()
+var formulaTenValues = [Double]()
+
+func run(with formula: ((Double) -> Double), arrayToAppend: inout [Double]) -> Double {
+    var previousApproximation = 2.0
+    var currentApproximation = 0.0
+    var nextApproximation = 1.0
+    
+    var k = 1.0
+    while (nextApproximation - currentApproximation).magnitude < (currentApproximation - previousApproximation).magnitude {
+        previousApproximation = formula(k)
+        currentApproximation = formula(k + 1)
+        nextApproximation = formula(k + 2)
+        k += 1
+        
+        arrayToAppend.append(currentApproximation)
+    }
+    
+    return k
 }
+
+run(with: formulaTwo(_:), arrayToAppend: &formulaTwoValues)
+run(with: formulaThree(_:), arrayToAppend: &formulaThreeValues)
+run(with: formulaTen(_:), arrayToAppend: &formulaTenValues)
+
+//String(format: "%.13f", currentApproximation)
+//print(previousApproximation)
+//print(currentApproximation)
+//print(k)
+//print(nextApproximation)
+
+//for k in 1...10 {
+//    print("Two: \(formulaTwo(Double(k)))")
+//    print("Three: \(formulaThree(Double(k)))")
+//    print("Ten: \(formulaTen(Double(k)))")
+//    print()
+//}
