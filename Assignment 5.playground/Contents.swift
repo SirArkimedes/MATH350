1441f3361e29b0feee120d5b2401e5fd4116a956
Andrew Robinson <sirarkimedes@gmail.com>
2018-11-13 12:26:22 -0600

    Basic formula calculation

--- Assignment 5.playground/Contents.swift
+++ Assignment 5.playground/Contents.swift
@@ -4,3 +4,31 @@
 /*************************************************/
 
 import Foundation
+
+func h(_ k: Double) -> Double {
+    return pow(10.0, -k)
+}
+
+func f(x: Double) -> Double {
+    return sin(pow(x, 3) - 7 * pow(x, 2) + 6 * x + 8)
+}
+
+let xo = (1.0 - sqrt(5)) / 3.0
+
+func formulaTwo(_ k: Double) -> Double {
+    return (f(x: xo + h(k)) - f(x: xo)) / h(k)
+}
+
+func formulaThree(_ k: Double) -> Double {
+    return (f(x: xo + h(k)) - f(x: xo - h(k))) / (2 * h(k))
+}
+
+func formulaTen(_ k: Double) -> Double {
+    return (-f(x: xo + 2 * h(k)) + 8 * f(x: xo + h(k)) - 8 * f(x: xo - h(k)) + f(x: xo - 2 * h(k))) / (12 * h(k))
+}
+for k in 1...10 {
+    print("Two: \(formulaTwo(Double(k)))")
+    print("Three: \(formulaThree(Double(k)))")
+    print("Ten: \(formulaTen(Double(k)))")
+    print()
+}
