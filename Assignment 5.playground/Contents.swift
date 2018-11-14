199ffb8fb228899cb59644238c4f0551b55ce121
Andrew Robinson <sirarkimedes@gmail.com>
2018-11-13 20:41:08 -0600

    Close enough for now

--- Assignment 5.playground/Contents.swift
+++ Assignment 5.playground/Contents.swift
@@ -37,7 +37,7 @@ func derivateOfF(x: Double) -> Double {
     return cos(pow(x, 3) - 7 * pow(x, 2) + 6 * x + 8) * (3 * pow(x, 2) - 14 * x + 6)
 }
 
-let xo = (1.0 - sqrt(5)) / 3.0
+let xo = (1.0 - sqrt(5.0)) / 3.0
 
 func formulaTwo(_ k: Double) -> Double {
     return (f(x: xo + h(k)) - f(x: xo)) / h(k)
@@ -56,12 +56,17 @@ func run(with formula: ((Double) -> Double)) -> Double {
     var currentApproximation = 0.0
     var nextApproximation = 1.0
     
-    var k = 1.0
+    var k = 0.0
     while (nextApproximation - currentApproximation).magnitude < (currentApproximation - previousApproximation).magnitude {
+        k += 1
         previousApproximation = formula(k)
         currentApproximation = formula(k + 1)
         nextApproximation = formula(k + 2)
-        k += 1
+
+
+        print((nextApproximation - currentApproximation).magnitude)
+        print((currentApproximation - previousApproximation).magnitude)
+        print((nextApproximation - currentApproximation).magnitude < (currentApproximation - previousApproximation).magnitude)
     }
     
     return k
@@ -75,9 +80,19 @@ let formulaTwoK = run(with: formulaTwo(_:))
 let formulaThreeK = run(with: formulaThree(_:))
 let formulaTenK = run(with: formulaTen(_:))
 
+
+// Printing... output.
+
+let separator = "|————————||——————————————————|——————————————————||——————————————————|——————————————————||——————————————————|——————————————————||"
+
+print(separator)
+print("|  Step  || Approximation by |   Error using    || Approximation by |   Error using    || Approximation by |   Error using    ||")
+print("|  Size  ||    formula (2)   |   formula (2)    ||    formula (3)   |   formula (3)    ||   formula (10)   |   formula (10)   ||")
+print(separator)
+
 for i in 1...Int(max(formulaTwoK, formulaThreeK, formulaTenK)) + 1 {
     let paddingLength = 16
-    let h = "10^\(i)".padding(toLength: 5, withPad: " ", startingAt: 0)
+    let h = "10^-\(i)".padding(toLength: 6, withPad: " ", startingAt: 0)
 
     let formulaTwoString = String(format: "%.13f", formulaTwo(Double(i))).leftPadding(toLength: paddingLength, withPad: " ")
     let errorTwo = String(format: "%.13f", derivateOfF(x: xo) - formulaTwo(Double(i))).leftPadding(toLength: paddingLength, withPad: " ")
@@ -88,5 +103,11 @@ for i in 1...Int(max(formulaTwoK, formulaThreeK, formulaTenK)) + 1 {
     let formulaTenString = String(format: "%.13f", formulaTen(Double(i))).leftPadding(toLength: paddingLength, withPad: " ")
     let errorTen = String(format: "%.13f", derivateOfF(x: xo) - formulaTen(Double(i))).leftPadding(toLength: paddingLength, withPad: " ")
 
-    print("| \(h) | \(formulaTwoString) | \(errorTwo) || \(formulaThreeString) | \(errorThree) || \(formulaTenString) | \(errorTen) ||")
+    print("| \(h) || \(formulaTwoString) | \(errorTwo) || \(formulaThreeString) | \(errorThree) || \(formulaTenString) | \(errorTen) ||")
 }
+print(separator)
+
+print()
+print("From (2):  f'(xo) ≈ \(String(format: "%.13f", formulaTwo(formulaTwoK))), where h = 10^-\(Int(formulaTwoK))")
+print("From (3):  f'(xo) ≈ \(String(format: "%.13f", formulaThree(formulaThreeK))), where h = 10^-\(Int(formulaThreeK))")
+print("From (10): f'(xo) ≈ \(String(format: "%.13f", formulaTen(formulaTenK))), where h = 10^-\(Int(formulaTenK))")
